e87160f67a0eb9c0fdd33e31625b098a6018dbbb
Pedro Castilho <cstl.px@gmail.com>
2021-02-10 01:49:38 -0300

    Fix syntax highlighting priorities

--- formality-mode.el
+++ formality-mode.el
@@ -14,11 +14,11 @@
 	"with"
 	"upto"))
 
-(setq formality-highlights
-      `((("[A-Z][a-z0-9_]*" . font-lock-type-face)
-	 (,(regexp-opt formality-keywords 'words) . font-lock-keyword-face)
-	 ("'.'" . font-lock-constant-face)
-	 ("[0-9]+[bsul]?" . font-lock-constant-face))))
+(setq formality-mode-highlights
+      `((("'.'" . font-lock-constant-face)
+	 ("[A-Z][a-z0-9_]*" . font-lock-type-face)
+	 ("[0-9]+[bsul]?" . font-lock-constant-face)
+	 (,(regexp-opt formality-keywords 'words) . font-lock-keyword-face))))
 
 (setq formality-mode-syntax-table
       (let ((table (make-syntax-table)))
@@ -26,6 +26,25 @@
 	(modify-syntax-entry ?\n "> b" table)
 	(modify-syntax-entry ?_ "w" table)
 	table))
+
+;; Indentation rules
+;; Top-level decls are type and typed decl
+;; indent-line
+;; If we are at line 1, indent to 0.
+;; Otherwise, define "previous line" as being (line - 1)
+;; If previous line starts with a top-level decl,
+;;   then indent by 2 spaces wrt previous line.
+;; If previous line starts with "let" or "def",
+;;   If contents of previous line are well-formed,
+;;     then indent at same level of previous line.
+;;   Otherwise, indent 2 spaces wrt previous line.
+;; If previous line starts with case and ends with {,
+;;   then indent 2 spaces wrt previous line.
+;; If previous line is a case,
+;;   If return value of the case is well-formed,
+;;     then indent at same level of previous line.
+;;   Otherwise, indent 2 spaces wrt previous line.
+;; If previous line is empty, indent wrt line above that.
 
 (defun formality-completion-at-point ()
   (interactive)
@@ -37,7 +56,7 @@
 (define-derived-mode formality-mode prog-mode "Formality"
   "Major mode for editing Formality code."
   (setq-local mode-name "Formality")
-  (setq-local font-lock-defaults formality-highlights)
+  (setq-local font-lock-defaults formality-mode-highlights)
   (setq-local comment-start "//")
   (setq-local comment-end "")
   (add-hook 'completion-at-point-functions
